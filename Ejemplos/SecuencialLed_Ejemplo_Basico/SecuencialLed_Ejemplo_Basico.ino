4a852a3df01c27e2458d0bdf64c3d63d0ff4fab5
Mandragora Tools <mandragoratools@gmail.com>
2016-12-24 19:43:07 +0100

    Add files via upload

---
+++ Ejemplos/SecuencialLed_Ejemplo_Basico/SecuencialLed_Ejemplo_Basico.ino
@@ -0,0 +1,31 @@
+//| SecuencialLed(Beta)>> Ejemplo básico "ascendente" y "descendente".     |
+//| Creado por: Mandragora Tools 2016                                      |                                                                                                            |
+//|________________________________________________________________________|
+
+
+#include <SecuencialLed.h>
+
+//Definimos los pines que usaremos para nuestros leds
+int leds[]={2,3,4,5,6,7,8,9,10};
+
+//Declaramos la instancia de SecuencialLed
+SecuencialLed led1;
+
+void setup() {
+
+//Inicializamos SecuencialLed pasandole la cantidad de leds y los pines correspondientes
+led1.ledPins(9,leds);
+
+//Declaramos la velocidad deseada (si esta no se especifica por defecto será de 1000 milisegundos)
+led1.velocidad(300);
+
+
+
+}
+
+void loop() {
+
+//Ejacutamos las secuencias
+led1.ascendente();
+led1.descendente();
+}
---
+++ Ejemplos/Secuencias_especiales/Secuencias_especiales.ino
@@ -0,0 +1,52 @@
+//| SecuencialLed(Beta)>> Ejemplo de secuencias especiales "cumulAsc" y "cumulDesc".                              |
+//| Creado por: Mandragora Tools 2016                                                                             |
+//|                                                                                                               |
+//| *Para usar estas secuencias conbinadas con otras es necesario hacer lo que muestra el ejemplo, con el objetivo|
+//| de que las secuencias sean completadas y no se crucen entre ellas.                                            |
+//|_______________________________________________________________________________________________________________|
+
+
+#include <SecuencialLed.h>
+
+//Declaramos dos variables de tipo boolean y a la que queramos que comience primero la iniciaremos con "true"
+bool b1=true;
+bool b2=false;
+
+//Definimos los pines que usaremos para nuestros leds
+int pins[]={2,3,4,5,6,7,8,9,10};
+
+//Declaramos la instancia de SecuencialLed
+SecuencialLed leds1;
+
+void setup() {
+
+//Inicializamos SecuencialLed pasandole la cantidad de leds y los pines correspondientes
+leds1.ledPins(9,pins);
+
+//Declaramos la velocidad deseada (si esta no se especifica por defecto será de 1000 milisegundos)
+leds1.velocidad(100);
+
+}
+
+void loop() {
+
+//Ejecutamos la secuencia cumulDesc cuando cumulAsc haya sido completada
+if(b1==true){
+if(leds1.cumulDesc()){
+b2=true;
+b1=false;
+}
+}
+
+//Ejecutamos la secuencia cumulAsc cuando cumulDesc haya sido completada
+if(b2==true){
+if(leds1.cumulAsc()){
+b1=true;
+b2=false;
+}
+}
+
+
+
+
+}
